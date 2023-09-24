f11cf183b957ea60eee0b0e9550258b2243ddd52
shivam5676 <techking08@gmail.com>
2023-09-24 00:29:23 -0700

    login component added with axios post request for loggin in account

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Login from './components/login/login';
 import SignUp from './components/signup/signup';
 
 function App() {
   return (
     <div className="App">
+      <Login></Login>
+      <hr></hr>
       <SignUp></SignUp>
     </div>
   );
